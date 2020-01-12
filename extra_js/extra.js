824df7903545ea60fcc3cdbaeac35d6245f71b30
minerupset <35759470+minerupset@users.noreply.github.com>
2020-01-11 20:45:25 -0800

    Deployed d6fc7ed with MkDocs version: 1.0.4

--- extra_js/extra.js
+++ extra_js/extra.js
@@ -32,6 +32,39 @@ const quotes = [
     author: "Marcus Aurelius"
   },
   {
+    quote: "An ounce of prevention is worth a pound of cure.",
+    author: "Benjamin Franklin"
+  },
+  {
+    quote:
+      "Sucking at something is the first step to being sort of good at something.",
+    author: "Jake the Dog"
+  },
+  {
+    quote:
+      "Show all your work on math problems, hide all your work in English papers.",
+    author: "Taylor Mali - What Teachers Make"
+  },
+  {
+    quote:
+      "If you and I exchange apples, we each have an apple. But if we exchange ideas, we each have two ideas.",
+    author: "Anonymous"
+  },
+  {
+    quote:
+      "A lump in the oatmeal, a lump in the throat, and a lump in the breast are not the same kind of lump. One needs to learn the difference.",
+    author: "Anonymous"
+  },
+  {
+    quote: "It does not do to dwell on dreams and forget to live.",
+    author: "Albus Percival Wulfric Brian Dumbledore"
+  },
+  {
+    quote:
+      "Out of the night that covers me, black as the pit from pole to pole, I thank whatever gods may be, for my unconquerable soul.",
+    author: "William Ernest Henley"
+  },
+  {
     quote:
       "Of course it is happening inside your head, Harry, but why on earth should that mean that it is not real?",
     author: "Albus Percival Wulfric Brian Dumbledore"
@@ -47,7 +80,7 @@ function addQuote() {
 }
 
 function sayHiToConsoleGoers() {
-  console.log(`Hello there, friend. I don't think you'll find too much interesting here, but it is good to look around to try and learn new things.
+  console.log(`Hello there, friend. I don't think you'll find too much interesting here, but it is good to look around to try and learn new things. If you're curious, this site is built with MkDocs, which has some great documentation at their site.
     
 Isn't it kinda cool that you're just sending 0s and 1s through fiber optic and copper wires looped all around the world, but the 0s and 1s are coming at a speed your computer is programmed to handle so it can be decoded into actual valuable information? Or random cat videos, whichever you prefer`);
 }
